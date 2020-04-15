73e2ea045b0bb359f47e20ee3296bdb0ae647566
Simon S <simon.handtoy@outlook.com>
2020-04-15 05:42:12 -0700

    Updates

---
+++ precache-manifest.49ded4c98bc90430ba18e0ab92bb8305.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc8eaea5f990b3f9f493d9d490dd54c6",
+    "url": "/arrangepuzzle/index.html"
+  },
+  {
+    "revision": "e88e59bb68fac619f966",
+    "url": "/arrangepuzzle/static/css/2.67123b20.chunk.css"
+  },
+  {
+    "revision": "cfbb5249014b41d71d38",
+    "url": "/arrangepuzzle/static/css/main.157d3def.chunk.css"
+  },
+  {
+    "revision": "e88e59bb68fac619f966",
+    "url": "/arrangepuzzle/static/js/2.cebe2aea.chunk.js"
+  },
+  {
+    "revision": "dff4b43b03c62c8d7a1cc6e27c878e76",
+    "url": "/arrangepuzzle/static/js/2.cebe2aea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cfbb5249014b41d71d38",
+    "url": "/arrangepuzzle/static/js/main.95d81ed4.chunk.js"
+  },
+  {
+    "revision": "7fe9e6c00547e81b2a32",
+    "url": "/arrangepuzzle/static/js/runtime-main.068896f1.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.f8983d338cc5ab3078ab1599dc4ae55f.js"
+  "/arrangepuzzle/precache-manifest.49ded4c98bc90430ba18e0ab92bb8305.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/arrangepuzzle/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
